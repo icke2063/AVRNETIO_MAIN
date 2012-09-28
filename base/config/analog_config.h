d104305679c5b985f9d9b8663903f02e23d0c39b
icke2063 <icke2063@gmail.com>
2012-09-28 15:01:44 +0200

    remove not yet reimplemented features from config

--- base/config/analog_config.h
+++ base/config/analog_config.h
@@ -4,11 +4,6 @@
 // AD-Wandler benutzen?
 	#define MINCHANNEL 4   //CHANNEL: 0..7 Beispiel MINCHANNEL:4 -> Pin 0-3 digitale Eingaenge OW:1-> Pin 7 reserviert >> ADC bei Pin 4,5,6
 	#define	ADC_PA		0x70
-
-//#if USE_OW
-//	#define ADC_COUNT 7-MINCHANNEL
-//#else
-//	#define ADC_COUNT 8-MINCHANNEL
-//#endif
+	#define ADC_COUNT 8-MINCHANNEL
 
 #endif //_ANALOG_CONFIG_H
